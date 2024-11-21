9c0abca586ecad1f4ec8486f152e2cea85963af3
milanaga <milanaga2000@gmail.com>
2024-11-21 20:07:13 +0530

    assignment answers of day 1 day 2

--- p1.c
+++ p1.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-// program to understand bitwise operators
 
-int main()
-{
-   int num = (float)((int)17.5 / 2.0);
-   printf("%f %d", num, num);
+int main() {
+    char weather1, weather2, weather3;
+
+
+    printf("Enter the first weather condition (r for raining, s for sunny, c for cloudy): ");
+    scanf(" %c", &weather1);
+    printf("Enter the second weather condition (r for raining, s for sunny, c for cloudy): ");
+    scanf(" %c", &weather2);
+    printf("Enter the third weather condition (r for raining, s for sunny, c for cloudy): ");
+    scanf(" %c", &weather3);
+
+    if ((weather1 == 's' && weather2 == 's' && weather3 != 'r' && weather3 != 'c') || 
+        (weather1 == 's' && weather2 == 'r' && weather3 != 'c')) {
+        printf("You can go out.\n");
+    } else {
+        printf("You cannot go out.\n");
+    }
+
 }
